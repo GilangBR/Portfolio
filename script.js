7ce98d7bea39136e767cea896ec8bed8bcfc0049
Canyou-GBR <Canyou-GBR@users.noreply.github.com>
2026-01-04 22:12:38 +0700

    Initial Portfolio Version

---
+++ script.js
@@ -0,0 +1,168 @@
+const header = document.querySelector('.main-header');
+
+const smMenuBtn = document.querySelector('.main-header__sm-scr-nav-btn')
+const smMenu = document.querySelector('.main-header__sm-menu')
+const smMenuCloseBtn = document.querySelector('.main-header__sm-menu-close')
+
+const smMenuLinks = document.querySelectorAll('.main-header__sm-menu-link')
+const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1')
+const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2')
+const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3')
+const smMenuLink4 = document.querySelector('.main-header__sm-menu-link--4')
+
+smMenuBtn.addEventListener('click', () => {
+  smMenu.style.transitionDelay = '0s'
+  smMenu.classList.add('main-header__sm-menu--active')
+
+  smMenuLink1.style.transitionDelay = '.5s'
+  smMenuLink1.style.transform = 'translateY(0)'
+  smMenuLink1.style.opacity = '1'
+
+  smMenuLink2.style.transitionDelay = '.8s'
+  smMenuLink2.style.transform = 'translateY(0)'
+  smMenuLink2.style.opacity = '1'
+
+  smMenuLink3.style.transitionDelay = '1.1s'
+  smMenuLink3.style.transform = 'translateY(0)'
+  smMenuLink3.style.opacity = '1'
+
+  smMenuLink4.style.transitionDelay = '1.4s'
+  smMenuLink4.style.transform = 'translateY(0)'
+  smMenuLink4.style.opacity = '1'
+})
+
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+smMenuLinks.forEach((ele) => {
+  ele.addEventListener('click', () => {
+    smMenuLink4.style.transitionDelay = '0s'
+    smMenuLink4.style.transform = 'translateY(50px)'
+    smMenuLink4.style.opacity = '0'
+
+    smMenuLink3.style.transitionDelay = '.3s'
+    smMenuLink3.style.transform = 'translateY(50px)'
+    smMenuLink3.style.opacity = '0'
+
+    smMenuLink2.style.transitionDelay = '.6s'
+    smMenuLink2.style.transform = 'translateY(50px)'
+    smMenuLink2.style.opacity = '0'
+
+    smMenuLink1.style.transitionDelay = '.9s'
+    smMenuLink1.style.transform = 'translateY(50px)'
+    smMenuLink1.style.opacity = '0'
+
+    smMenu.style.transitionDelay = '1.2s'
+    smMenu.classList.remove('main-header__sm-menu--active')
+
+    setTimeout(() => {
+      document.getElementById(ele.name).scrollIntoView()
+    }, 1300)
+  })
+})
+
+smMenuCloseBtn.addEventListener('click', () => {
+  smMenuLink4.style.transitionDelay = '0s'
+  smMenuLink4.style.transform = 'translateY(50px)'
+  smMenuLink4.style.opacity = '0'
+
+  smMenuLink3.style.transitionDelay = '.3s'
+  smMenuLink3.style.transform = 'translateY(50px)'
+  smMenuLink3.style.opacity = '0'
+
+  smMenuLink2.style.transitionDelay = '.6s'
+  smMenuLink2.style.transform = 'translateY(50px)'
+  smMenuLink2.style.opacity = '0'
+
+  smMenuLink1.style.transitionDelay = '.9s'
+  smMenuLink1.style.transform = 'translateY(50px)'
+  smMenuLink1.style.opacity = '0'
+
+  smMenu.style.transitionDelay = '1.2s'
+  smMenu.classList.remove('main-header__sm-menu--active')
+})
+
+
+
+
+
+// ---
+const themeColorSelector = document.querySelector('.themeClrSelector')
+const themeColorSelectorInput = document.querySelector(
+  '.themeClrSelector__input'
+)
+const root = document.documentElement;
+
+
+
+const hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result
+    ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16),
+    }
+    : null
+}
+
+const eventFire = (el, etype) => {
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype)
+  } else {
+    let evObj = document.createEvent('Events')
+    evObj.initEvent(etype, true, false)
+    el.dispatchEvent(evObj)
+  }
+}
+
+themeColorSelector.addEventListener('click', () => {
+  eventFire(themeColorSelectorInput, 'input')
+})
+
+const setDynamicColor = (color) => {
+
+  const { r, g, b } = hexToRgb(`${color}`)
+
+  root.style.setProperty('--themeColor', `${r},${g},${b}`);
+  //localStorage.setItem('color', color)
+}
+
+themeColorSelectorInput.addEventListener('input', (e) => {
+  setDynamicColor(e.target.value)
+})
+
+// if (localStorage.getItem('color')) {
+//   let userSelectedColor = localStorage.getItem('color')
+//   themeColorSelectorInput.value = userSelectedColor
+//   setDynamicColor(userSelectedColor)
+// }
+
+// ---
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const homeProjectBtn = document.getElementById('homeProjectBtn');
+  const headerLogoContainer = document.querySelector('.main-header__logo-container')
+
+  if (homeProjectBtn) {
+    homeProjectBtn.addEventListener('click', function () {
+      window.location.href = './index.html#projects';
+    });
+  }
+
+  if (headerLogoContainer) {
+    headerLogoContainer.addEventListener('click', function () {
+      window.location.href = 'index.html';
+    });
+  }
+});
+
